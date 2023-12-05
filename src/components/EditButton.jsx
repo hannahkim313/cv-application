344597f81d4fd1bc66a338139e9e37f188f8dfda
Hannah Kim <67137839+hannahkim313@users.noreply.github.com>
2023-12-04 16:00:19 -0800

    Create an EditButton component

---
+++ src/components/EditButton.jsx
@@ -0,0 +1,8 @@
+import editIcon from '../assets/images/edit.svg';
+import Button from './Button';
+
+const EditButton = () => (
+  <Button src={editIcon} alt="" text="Edit CV" className="edit" />
+);
+
+export default EditButton;
